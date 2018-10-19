3c622a09229a8828f2d6f017cc4691b695625341
lamnvs <lam.nvs.13@gmail.com>
2018-10-19 11:23:03 +0700

    Fix lỗi 2 ô cách nhau 1 đơn vị

--- src/algorithms/Matching.java
+++ src/algorithms/Matching.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class Matching {
     private static boolean checkLineX(int[][] map, int y1, int y2, int x) {
-        if(y1 == y2) return true;
+//        if(y1 == y2) return true;
         int min = Math.min(y1, y2);
         int max = Math.max(y1, y2);
-        for (int y = min + 1; y <= max; y++){
-            if (y == max) return true;
+        for (int y = min + 1; y < max; y++){
             if(map[x][y] != 0){
                 return false;
             }
@@ -16,11 +15,10 @@ public class Matching {
         return true;
     }
     private static boolean checkLineY(int[][]map, int x1, int x2, int y) {
-        if(x1==x2) return  true;
+       // if(x1==x2) return  true;
         int min = Math.min(x1, x2);
         int max = Math.max(x1, x2);
-        for (int x = min + 1; x <= max; x++){
-            if (x==max) return true;
+        for (int x = min + 1; x < max; x++){
             if(map[x][y] != 0){
                 return false;
             }
@@ -29,6 +27,7 @@ public class Matching {
     }
 
     private static int checkRectX(int[][] map, int x1, int y1, int x2, int y2){
+        if(x1 == x2) return -1;
         int[] yMinPoint;
         int[] yMaxPoint;
         if (y1 > y2) {
@@ -47,6 +46,7 @@ public class Matching {
         return -1;
     }
     private static int checkRectY(int[][] map, int x1, int y1, int x2, int y2){
+        if (y1 == y2) return -1;
         int[] xMinPoint;
         int[] xMaxPoint;
         if (x1 > x2) {
@@ -171,11 +171,10 @@ public class Matching {
         int y2 = scan.nextInt();
         System.out.println(map[x1][y1]);
         System.out.println(map[x2][y2]);
-        System.out.println("Nam");
         int[][] route = checkTwoPoint(map, x1, y1, x2, y2);
         System.out.println(route[0][0]);
         System.out.println(route[0][1]);
         System.out.println(route[1][0]);
         System.out.println(route[1][1]);
-    }
+        }
 }
